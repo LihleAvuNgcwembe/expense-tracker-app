68b25a4c47305236131364a22e672744e9a70b44
LihleAvuNgcwembe <ngcwembelihle@gmail.com>
2024-10-29 15:26:27 +0200

    Create Expense Repository, ExpenseDto, and ExpenseMapper

---
+++ src/main/java/net/javaguides/expense/mapper/ExpenseMapper.java
@@ -0,0 +1,36 @@
+package net.javaguides.expense.mapper;
+
+import net.javaguides.expense.dto.CategoryDto;
+import net.javaguides.expense.dto.ExpenseDto;
+import net.javaguides.expense.entity.Category;
+import net.javaguides.expense.entity.Expense;
+
+public class ExpenseMapper {
+
+    // map from Expense entity to ExpenseDto
+    public static ExpenseDto mapToExpenseDto(Expense expense){
+        return new ExpenseDto(
+                expense.getId(),
+                expense.getAmount(),
+                expense.getExpenseDate(),
+                new CategoryDto(
+                        expense.getCategory().getId(),
+                        expense.getCategory().getName()
+                )
+        );
+    }
+
+    // map from ExpenseDto to Expense entity
+    public static Expense mapToExpense(ExpenseDto expenseDto){
+        Category category =  new Category();
+        category.setId(expenseDto.id());
+
+
+        return new Expense(
+                expenseDto.id(),
+                expenseDto.amount(),
+                expenseDto.expenseDate(),
+                category
+        );
+    }
+}
